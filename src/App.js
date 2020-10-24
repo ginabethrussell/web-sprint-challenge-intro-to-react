f83130d8fa5e3272546ac89101ef7255790f1f95
Gina Beth Russell <ginabethrussell@Ginas-MacBook-Pro.local>
2020-10-24 10:06:19 -0400

    added api call, created state, rendered character info on character components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,53 @@
-import React from 'react';
+import Axios from 'axios';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
+import Character from './components/Character';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 75%;
+  max-width: 960px;
+  margin: 0 auto;
+  `;
+const CharacterDisplay = styled.div`
+  display: flex;
+  flexWrap: wrap;
+  justifyContent: space-evenly;
+  align-content: space-around`;
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+  // Set up state to hold character data from Star Wars API
+  const [characterData, setCharacterData] = useState([]);
+
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(()=>{
+    axios.get('https://swapi.dev/api/people/')
+      .then(response => {
+        console.log(response.data);
+        setCharacterData(response.data.results);
+      })
+      .catch(err => console.log(err))
+  },[]);
+
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <Wrapper >
+        <h1 className="Header">Star Wars Characters</h1>
+        <CharacterDisplay >
+        {
+          characterData.map(character => (
+            <Character key={character.created} swData={character} />
+          ))
+        }
+        </CharacterDisplay>
+      </Wrapper>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,38 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const CharacterCard = styled.div`
+    width: 30%;
+    border: 2px solid black;
+    background-color: white;
+    color: black;
+    `;
+
+const CardTitle = styled.h2`
+    font-size: 2 rem;
+    `;
+
+const CardInfo = styled.ul`
+    listType = none;
+    `;
+export default function Character(props){
+    const {swData} = props;
+
+    return(
+        <CharacterCard>
+            <CardTitle>{swData.name}</CardTitle>
+            <CardInfo>
+                <li>Birth Year: {swData.birth_year}</li>
+                <li>Gender: {swData.gender}</li>
+                <li>Height: {swData.height}</li>
+                <li>Mass: {swData.mass}</li>
+                <li>Hair color: {swData.hair_color}</li>
+                <li>Eye color: {swData.eye_color}</li>
+                <li>Skin color: {swData.skin_color}</li>
+            </CardInfo>
+
+        </CharacterCard>
+    )
+
+}
